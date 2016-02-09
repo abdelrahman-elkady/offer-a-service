db58e5ac165538186fa37219a6c0912b58ffd9bb
kady <elkadyabdelrahman@gmail.com>
2016-02-10 00:30:58 +0200

    Removing offers created by the user

--- client/templates/offer.js
+++ client/templates/offer.js
@@ -3,3 +3,20 @@ Template.offer.rendered = function() {
     position: 'right center'
   });
 };
+
+Template.offer.helpers({
+  isOwner: function() {
+    return this.owner === Meteor.userId();
+  }
+});
+
+
+Template.offer.events({
+  "click .ui.remove.icon": function(event) {
+    console.log("CLICKED" + this._id);
+    event.preventDefault();
+
+    Meteor.call("deleteOffer", this._id);
+
+  }
+});
